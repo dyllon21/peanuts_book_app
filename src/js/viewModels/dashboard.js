0c6e7fddb0f52981b9aa8351364d6bdce9e0e41f
Zikisa Cale <zikisacalesogaxa@gmail.com>
2017-11-22 16:02:44 +0200

    dashboard.js changes on some of the code we do not need

--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -1,85 +1,41 @@
-define(['ojs/ojcore', 'knockout', 'jquery', 'ais', 'ds', 'jet-composites/filter-table/loader', 'jet-composites/overview-card/loader'],
-  function (oj, ko, $, ais, ds) {
+define(['ojs/ojcore', 'knockout', 'jquery', 'ais', 'ds', 'jet-composites/filter-table/loader'],
+  function(oj, ko, $, ais, ds) {
+
+    function Book(author, title, genre, about) {
+      this.author = author;
+      this.title = title;
+      this.genre = genre;
+      this.about = about;
+    }
 
     function DashboardViewModel() {
       var self = this;
       // check ds is initialized
-      self.itemsArray = ko.observable([]);
+      self.books = ko.observable([]);
       // for web component pattern
       // data service delivers for filter-table component --- data prop requirements
 
-
-      self.itemData = ds.getItemsStatic();
-      ds.init().then(function (data) {
-        console.log(data)
-        // for JQUERY injection pattern - below in hamdleAttached method
-
-
-        // for KO Pattern - fetchdata into observable
-
-
-        ds.getItemsAPI().then(function (data2) {
-          console.log(JSON.stringify(data2.rows))
-          self.itemsArray(data2.rows);
-
+      $.getJSON("http://peanut-library-api.herokuapp.com/api/v1/books", (allData) => {
+        let allBooks = allData.books;
+        var mappedTasks = $.map(allBooks, (book) => {
+          console.log(book);
+          return new Book(book.author, book.bookName, book.genre, book.about);
         });
-
-
-
-      })
-
-
-      // event handler - provides jquery and jquery ui event objects
-      self.handleRowClick = function (evt, ui) {
-        console.log($(this));
-        //console.log("row clicked == "+evt.target.id); // this is table ID
-        console.log("row clicked == " + JSON.stringify(ui)); // startIndex holds row
-
-      };
-
-      // setup obeservables for overview-card
-      self.cachedNumber = ko.observable({
-        count: 5
+        self.books(mappedTasks);
       });
 
-      // component life cycle methods
-      self.handleActivated = function (info) {
+      self.handleAttached = function(info) {
         // Implement if needed
-        console.log("dashboard viewModel activated : " + info); // this is the entire view html object at this state in the lifecycle
-      };
-
-      self.handleAttached = function (info) {
-
-
-        DOM is ready
-        can use jQuery to inject our table html
-        const tableEl = $("#myTarget");
-        $.each(self.itemsArray(), function (i,o){
-          //console.log(JSON.stringify(o))
-          const kys = Object.keys(o);
-          $.each(kys, function(idx,obj) {
-            tableEl.append('<tr><td>'+obj+'</td><td>'+o[obj]+'</td></tr>')
-          });
-
-        })
-
-
-        inject custom components
-        for(var x=0;x<15;x++){
-          $('#appHolder').append('<overview-card name="name" maincolor="#c1c1c1" highlight="#B3CD62" observables="{{cachedNumber}}" clicked="{{handleRowClick}}" icon="home-icon-24"></overview-card>')
-        }
-
-
       };
 
 
-      self.handleBindingsApplied = function (info) {
+      self.handleBindingsApplied = function(info) {
         // Implement if needed
 
       };
 
 
-      self.handleDetached = function (info) {
+      self.handleDetached = function(info) {
         // Implement if needed
 
       };
@@ -91,5 +47,4 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ais', 'ds', 'jet-composites/filter-
      * only one instance of the ViewModel is needed.
      */
     return new DashboardViewModel();
-  }
-);
+  });
