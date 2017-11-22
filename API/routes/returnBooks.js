f51d31190d3e3fb287e565d3be25c0e152ad9a71
Callum Fraser <callum@projectcodex.co>
2017-11-22 12:19:43 +0200

    added returnbooks update functionality

--- API/routes/returnBooks.js
+++ API/routes/returnBooks.js
@@ -7,19 +7,39 @@ const router = express.Router();
 router.put('/:_id', (req, res) => {
     let code = res.statusCode;
     var bookReturned = req.body;
+    var bookId = req.params.id;
+    var recommendationCheck = 0;
 
-    var update = {
-       $inc : {timesTaken : 1 },
+    if (bookReturned.recommendation == true){
+      recommendationCheck = 1;
+    }
 
-    };
-
-    console.log(updateInfo);
+    console.log(bookReturned);
 
     bookModel.findOneAndUpdate({
-      "id":bookReturned._id
+      "id":bookId
     }, {
-
-    })
+     $inc : {
+       recommendations : (0 + recommendationCheck)
+     },
+     availability: true,
+     currentUser : "None"
+   }, {
+     upsert : false
+   }, function(err,result){
+     if (err){
+       return res.json({
+         status : 'error',
+         error : err,
+         taken : []
+       })
+     } else {
+       res.json({
+         status : "success",
+         taken : result
+       })
+     }
+   })
 
 });
 
